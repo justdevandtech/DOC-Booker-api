63afe71b69286275092d2f8c0d1881fbf3e2b2e7
ophicial-pauloski <pasukwo5@gmail.com>
2023-06-29 07:30:15 +0100

    feat: 🎸 update

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
   try {
-   mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
     console.log(`MongoDB connected successfully`);
   } catch (error) {
     console.log(error);
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import "dotenv/config.js";
 import { dbConnection } from "./config/db.js";
-
-//mongodb connection
-dbConnection();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -14,17 +11,20 @@ import { user_router } from "./routes/userRoute.js";
 import { doctor_router } from "./routes/doctorRoute.js";
 
 //middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.json());
 app.use("/api/auth", user_router);
 app.use("/api/doctor", doctor_router);
 
-
 app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to DOC-Booker API"
-    })
-})
+  res.json({
+    message: "Welcome to DOC-Booker API",
+  });
+});
 
-app.listen(PORT, () => console.log("server running on port " + PORT));
+//Connect to the database before listening
+dbConnection().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests on port " + PORT);
+  });
+});
